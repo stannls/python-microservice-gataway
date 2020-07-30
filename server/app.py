ae52453aab91e00520af43b6d83b41f777ace1fc
Yannis Storrer <yannis.storrer@web.de>
2020-07-30 12:55:23 +0200

    Removed commented code

--- server/app.py
+++ server/app.py
@@ -9,15 +9,6 @@ configFile = open('config.yml')
 config = yaml.load(configFile, Loader=yaml.FullLoader)
 logging.basicConfig(level=config["loglevel"])
 
-
-# start_server = websockets.serve(server, "0.0.0.0", 8000)
-# logging.info('Starting server...')
-# asyncio.get_event_loop().run_until_complete(start_server)
-# asyncio.get_event_loop().run_forever()
-
-
-
-
 server = WebsocketServer(8000, host='0.0.0.0')
 server.set_fn_new_client(new_client)
 server.set_fn_message_received(new_message)
