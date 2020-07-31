efcdf6ae4588e514bb7d433f0d745cf3b9bd07ef
Yannis Storrer <yannis.storrer@web.de>
2020-07-31 11:03:47 +0200

    Added buggy client implementation

--- server/modules/server.py
+++ server/modules/server.py
@@ -6,7 +6,15 @@ import time
 import _thread as thread
 
 microservices = {}
-clients = {}
+microservice_connections = {}
+client_Connections = {}
+
+
+# I know very bad code
+def getClientConnectionID(dict, uuid):
+    for i in dict.items():
+        if i[1]["uuid"] == uuid:
+            return i[0]
 
 
 def new_client(client, server):
@@ -34,10 +42,9 @@ def new_message(client, server, message):
                                                                                  "description"],
                                                                              endpoints=request_object["data"][
                                                                                  "endpoints"])
-                clients[client["id"]] = microservices[request_object["data"]["name"]]
+                microservice_connections[client["id"]] = microservices[request_object["data"]["name"]]
                 server.send_message(client, json.dumps({"code": 200, "connected": True}))
                 logging.info("Registered new microservice " + request_object["data"]["name"])
-
                 def run():
                     while microservices[request_object["data"]["name"]].lock is False:
                         if microservices[request_object["data"]["name"]].check_queue():
@@ -56,6 +63,17 @@ def new_message(client, server, message):
                                     queueLen = len(microservices[request_object["data"]["name"]].queue)
 
                                 time.sleep(0.5)
+                                for i in client_Connections.items():
+                                    if i[1]["connected"] is False:
+                                        logging.info("Sending deletion")
+                                        server.send_message(client, json.dumps({
+                                            "code": 200,
+                                            "name": request_object["data"]["name"],
+                                            "endpoint": microservices[request_object["data"]["name"]].queue[microservices[request_object["data"]["name"]].showQueuePosition(i[1]["uuid"])]["request"]["endpoint"],
+                                            "type": "deletion",
+                                            "uuid": i[1]["uuid"]
+                                        }))
+                                        del microservices[request_object["data"]["name"]].queue[microservices[request_object["data"]["name"]].showQueuePosition(i[1]["uuid"])]
                         time.sleep(0.5)
 
                 thread.start_new_thread(run, ())
@@ -68,6 +86,7 @@ def new_message(client, server, message):
                     queueID = microservices[request_object["name"]].append_queue(endpoint=request_object["endpoint"],
                                                                                  parameters=request_object["data"],
                                                                                  queueID=request_object["uuid"])
+                    client_Connections[client["id"]] = {"uuid": request_object["uuid"], "connected": True}
                     while not microservices[request_object["name"]].check_queue_response(
                             position=microservices[request_object["name"]].showQueuePosition(queueID)):
                         time.sleep(0.5)
@@ -75,13 +94,13 @@ def new_message(client, server, message):
                     resp = microservices[request_object["name"]].show_queue_response(
                         position=microservices[request_object["name"]].showQueuePosition(queueID))
                     server.send_message(client, resp)
-                    while microservices[request_object["name"]].lock is False:
+                    while microservices[request_object["name"]].lock is False and client_Connections[
+                        client["id"]]["connected"]:
                         if microservices[request_object["name"]].show_queue_response(
                                 position=microservices[request_object["name"]].showQueuePosition(queueID)) != resp:
                             resp = microservices[request_object["name"]].show_queue_response(
                                 position=microservices[request_object["name"]].showQueuePosition(queueID))
                             server.send_message(client, resp)
-                        time.sleep(0.5)
                 else:
                     server.send_message(client, json.dumps({"code": 400}))
                     logging.debug("Got bad request")
@@ -99,6 +118,9 @@ def new_message(client, server, message):
 
 def new_disconnect(client, server):
     logging.warning("New disconnect")
-    if client["id"] in clients:
-        clients[client["id"]].lock = True
-        del clients[client["id"]]
+    if client["id"] in microservice_connections:
+        microservice_connections[client["id"]].lock = True
+        time.sleep(0.5)
+        del microservice_connections[client["id"]]
+    elif client["id"] in client_Connections:
+        client_Connections[client["id"]]["connected"] = False
